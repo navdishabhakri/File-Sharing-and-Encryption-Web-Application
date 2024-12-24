8b8bd1c9a98ecd4abac52fb913c5259ef22a3f82
navdishabhakri <144282077+navdishabhakri@users.noreply.github.com>
2024-12-24 16:57:43 -0500

    Update forms.py

--- forms.py
+++ forms.py
@@ -2,14 +2,8 @@
 """
 ############################
 #  File Name: forms.py
-#  Group Number: 4
-#  Group Members Names : abdul wadood mohammed, navdisha bhakri
-#  Group Members Seneca Email : awmohammed3@myseneca.ca, nbhakri@myseneca.ca
 #  Date : 20 November 2024
-#  Authenticity Declaration :
-#  I declare this submission i s the result of our group work and has not been
-#  shared with any other groups/students or 3rd party content provider. This submitted
-#  piece of work is entirely of my own creation.
+
 ############################
 """
 
