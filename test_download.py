1651068e8bca5b9f61b2db6af0367d09d99954c5
navdishabhakri <144282077+navdishabhakri@users.noreply.github.com>
2024-12-24 16:59:43 -0500

    Update test_download.py

--- test_download.py
+++ test_download.py
@@ -1,12 +1,5 @@
 #  File Name: test_download.py
-#  Group Number: 4
-#  Group Members Names : abdul wadood mohammed, navdisha bhakri
-#  Group Members Seneca Email : awmohammed3@myseneca.ca, nbhakri@myseneca.ca
 #  Date : 6 december 2024
-#  Authenticity Declaration :
-#  I declare this submission i s the result of our group work and has not been
-#  shared with any other groups/students or 3rd party content provider. This submitted
-#  piece of work is entirely of my own creation.
 
 import pytest
 from django.urls import reverse
